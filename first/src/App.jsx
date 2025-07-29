4b90eee60cce8f40a97523fc99be8f8c5049d496
RAJ <razkumarji27@gmail.com>
2025-07-29 17:07:39 +0000

    navbar

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react'
 
 import Form from './Form'
+import { Navbar } from './Navbar'
 
 const App = () => {
   
@@ -14,6 +15,7 @@ const App = () => {
     <h1 className='bg-red-900 text-2xl h-40 w-60 px-10 py-10 my-2 mx-2'>My age is {age} </h1>
     <button onClick={onchange} className='bg-green-700 text-xl h-10 w-30 mx-2 my-2 '>click me</button>
     <Form/>
+    <Navbar/>
     </>
     
   )
---
+++ first/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+    <nav className='flex space-x-80 list-none'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+    </nav>
+    </>
+  )
+}
