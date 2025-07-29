28be2fce0a188b373d5951ed0d0c7088f802db2f
RAJ <razkumarji27@gmail.com>
2025-07-29 17:33:37 +0000

    formhandler

--- first/src/Form.jsx
+++ first/src/Form.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const[username,setUsername]=useState('')
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log(username)
+    setUsername('')
+
+  }
   return (
     <>
-    <form>
-      <input className='bg-yellow-100 mx-5 w-60 h-20' type='text' placeholder='type anything'></input>
+    <form onSubmit={(e)=>{
+      submitHandler(e)
+    }}>
+      <input value={username}
+      onChange={(e)=>{
+        setUsername(e.target.value)
+      }}
+      className='bg-yellow-100 mx-5 w-60 h-20' type='text' placeholder='Enter Your Name'></input>
       <button onClick={()=>
         console.log("hello")
       } className='bg-pink-400 text-black w-40 h-30'>click me </button>
